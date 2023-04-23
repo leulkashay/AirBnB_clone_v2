a49b500401af1ef186c75ebcab782fe72d194474
leulkashay <leulkashay2012@gmail.com>
2023-04-22 20:38:46 -0700

    task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Flask web framework
-   """
+"""
 from  flask import Flask
 
 app = Flask(__name__)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Flask web framework"""
+"""Flask web framework
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
