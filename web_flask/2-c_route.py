2e66523f82b213ded84991756d4959bc91b7cdd6
leulkashay <leulkashay2012@gmail.com>
2023-04-23 00:15:13 -0700

    task

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-"""Flask framewoek
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
-from flask import Flaskq
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_world():
-    """returm hello hbnb
-    """
-    return "Hello HBNB"
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
-def HBNB():
-    """return HBNB"""
+def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
-def text(text):
-    """return text given"""
-    return "C {}".format(text.replace("_"," "))
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-if __name__ =="__main___":
-    app.run()
-
-                         
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
