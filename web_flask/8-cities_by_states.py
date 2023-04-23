22ec64936e6f7d534c31bae744cdf55c8f3bc268
leulkashay <leulkashay2012@gmail.com>
2023-04-23 07:45:54 -0700

    new task

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,30 @@
-#!/usr/bin/python3
-"""starts a flask web application """
+#!/usr/bin/env python3 
+""" Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
 
-from flask import Flask, render_template
-import models
+app = Flask(__name__)
 
-app = Flask("__name__")
-
-@app.teardown_appcontext
-def refresh(exception):
-    models.storage.close()
-
-
-@app.route("/states_list", strict_slashes=False)
-def route_states():
-    pep.fix = models.dummy_classes["State"]
-    data = models.storage.all(cls = pep.fix)
-    states = data.values()
-    return render.template('7-states_list.html', states_list=states)
 
 @app.route("/cities_by_states", strict_slashes=False)
-def route_city():
-    pep_fix = models.dummy.classes["state"]
-    data = models.storage.all(cls = pep.fix)
-    states = data.values()
-    return render_template("8-cities_by_states.html", states_list=states)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
 
 
-if __name__ =="__main__":
-    app.run()
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
